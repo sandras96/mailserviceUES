386ccc7e052a79823c96ed4a28d4178fa93c9999
sandras96 <sandramish96@gmail.com>
2020-07-21 19:33:12 +0200

    first commit

---
+++ mailservice/src/test/java/com/myproject/mailservice/mailservice/AppTest.java
@@ -0,0 +1,20 @@
+package com.myproject.mailservice.mailservice;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
